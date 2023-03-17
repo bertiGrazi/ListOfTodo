1c7ef8750438d5eeb8633c784f93c2cae563b1eb
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-03-17 11:27:59 -0300

    create app #0

---
+++ TodoList/TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Grazi  Berti on 17/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Grazi  Berti on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
