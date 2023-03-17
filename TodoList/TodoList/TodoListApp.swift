69afdf79ec858c14d62c228b57581a0203b7df6f
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-03-17 14:48:43 -0300

    #03: Create ListViewModel and called at ListView

--- TodoList/TodoList/TodoListApp.swift
+++ TodoList/TodoList/TodoListApp.swift
@@ -19,11 +19,14 @@ import SwiftUI
 
 @main
 struct TodoListApp: App {
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 ListView()
             }
+            .environmentObject(listViewModel)
         }
     }
 }
